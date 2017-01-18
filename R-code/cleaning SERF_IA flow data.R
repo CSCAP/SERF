e16a8a3a893c63fe802c9df233132408f626c3a1
IASTATE\gio <giorgichi@gmail.com>
2017-01-18 17:48:12 -0600

    add plot 1 to clean

--- R-code/cleaning SERF_IA flow data.R
+++ R-code/cleaning SERF_IA flow data.R
@@ -6,21 +6,23 @@ library(zoo)
 
 setwd("C:/Users/gio/Documents/CSCAP/Data/SERF_IA/Tile Flow")
 
+p1 <- read_excel("~/CSCAP/Data/SERF_IA/Tile Flow/SERF Drainage Data 07 to 15.xlsx", sheet = "Plot1")
 p2 <- read_excel("~/CSCAP/Data/SERF_IA/Tile Flow/SERF Drainage Data 07 to 15.xlsx", sheet = "Plot2")
 p3 <- read_excel("~/CSCAP/Data/SERF_IA/Tile Flow/SERF Drainage Data 07 to 15.xlsx", sheet = "Plot3")
 p4 <- read_excel("~/CSCAP/Data/SERF_IA/Tile Flow/SERF Drainage Data 07 to 15.xlsx", sheet = "Plot4")
 p5 <- read_excel("~/CSCAP/Data/SERF_IA/Tile Flow/SERF Drainage Data 07 to 15.xlsx", sheet = "Plot5")
 p6 <- read_excel("~/CSCAP/Data/SERF_IA/Tile Flow/SERF Drainage Data 07 to 15.xlsx", sheet = "Plot6")
 
-p2$plot <- "Plot 2"
-p3$plot <- "Plot 3"
-p4$plot <- "Plot 4"
-p5$plot <- "Plot 5"
-p6$plot <- "Plot 6"
+p1$plot <- "plot1"
+p2$plot <- "plot2"
+p3$plot <- "plot3"
+p4$plot <- "plot4"
+p5$plot <- "plot5"
+p6$plot <- "plot6"
 
 
 # combine data and add timestamp
-bind_rows(p2, p3, p4, p5, p6) %>% 
+bind_rows(p1, p2, p3, p4, p5, p6) %>% 
   filter(Year > 2010) %>%
   select(plot, Year, Month, Day, Time, `Incremental drainage, mm`, `Nitrate loss, kg/ha`) %>%
   mutate(Time = round_date(Time, "mins")) %>%
@@ -31,12 +33,31 @@ bind_rows(p2, p3, p4, p5, p6) %>%
 names(loss) <- c("plot", "year", "month", "day", "time", "drainage_mm", "n_loss", "timestamp")
 
 
-# round time to nearest 15 min 
+# find non-consequtive dates
 loss %>%
+  mutate(diff = c(1800, diff(timestamp))/60) -> loss
+loss[which(loss$diff < 0 & loss$diff > -60*24*365*4), ]
+# there are three non-consequtive entries 
+# look at those values in the "context" (with neighboring data)
+which(loss$diff < 0 & loss$diff > -60*24*365*4) -> tempo
+loss[rep(tempo[1], each = 10) + (-8:1), ]  # remove this row
+loss[rep(tempo[2], each = 10) + (-8:1), ]  # need to fix "month" (replace 1 with 3)
+loss[rep(tempo[3], each = 10) + (-5:4), ]  # need to fix "time" (should be 7:46 instead of 3:46)
+
+loss[tempo[3], "time"] <- "07:46"
+loss[tempo[2], "month"] <- 3
+loss <- loss[-tempo[1], ]
+
+# redo timestamp to fix corrected values and add rounded time to the nearest 15 min
+loss %>%
+  mutate(timestamp = parse_date_time(paste(paste(year, month, day, sep = "-"), time), "ymd HM")) %>%
+  mutate(diff = c(1800, diff(timestamp))/60) %>% 
   mutate(roundtime = as.numeric(timestamp)) %>%
   mutate(roundtime = round(roundtime/(15*60))*(15*60)) %>%  # round to 15 min intervals
   mutate(roundtime = as.POSIXct(roundtime, origin = "1970-01-01", tz = "UTC")) %>% # convert back to dym hms
   mutate(timediff = difftime(roundtime, timestamp, units = "mins")) -> loss
+
+
 
 # see when conscutive round times are the same (not different)
 loss %>% 
@@ -53,11 +74,12 @@ loss[rep(which(diff(loss$roundtime) == 0), each =3) + (-1:1), ]
 loss[-(which(diff(loss$roundtime)==0)+1), ] -> loss
 
 save(loss, file = "C:/Users/Gio/Documents/GitHub/CSCAP/Sustainable_Corn_Paper/Data/flow/SERF/no3_loss.Rda")
-load(file = "C:/Users/Gio/Documents/GitHub/CSCAP/Sustainable_Corn_Paper/Data/flow/SERF/no3_loss.Rda")
+#load(file = "C:/Users/Gio/Documents/GitHub/CSCAP/Sustainable_Corn_Paper/Data/flow/SERF/no3_loss.Rda")
+
 
 loss %>% 
   arrange(plot, roundtime) %>%
-  mutate(timediff = c(1800, diff(roundtime))/60) -> loss
+  mutate(timediff = c(30, diff(roundtime))) -> loss
 
 # find measurement frequency 
 loss[which(loss$timediff == 15) %>% rep(each = 5) + (-2:2), ]
@@ -72,18 +94,13 @@ which(loss$timediff == 15 & loss$year == 2011) -> tempoRow
 # instead of 30 min in 2011 data;
 # code below should fix this (see row 2992 for more info)
 
-loss[loss$plot == "Plot 2" & loss$year == 2011 & loss$roundtime > tempo$roundtime, "roundtime"] <-
-  loss[loss$plot == "Plot 2" & loss$year == 2011 & loss$roundtime > tempo$roundtime, "roundtime"] - duration(mins = 15)
+loss[loss$plot == "plot2" & loss$year == 2011 & loss$roundtime > tempo$roundtime, "roundtime"] <-
+  loss[loss$plot == "plot2" & loss$year == 2011 & loss$roundtime > tempo$roundtime, "roundtime"] - duration(mins = 15)
 
 loss <- loss[-tempoRow, ]
 
 # find more duplicates and fix
-loss %>% 
-  filter(timediff == 0)
-loss[rep(which(loss$timediff == 0), each =5) + (-2:2), ]
-loss[-which(loss$timediff == 0 & loss$plot == "Plot 4"), ] -> loss
-loss[which(loss$timediff == 0)-1, "roundtime"] <- loss[which(loss$timediff == 0)-1, "roundtime"] - duration(mins = 30)
-
+loss %>% filter(timediff == 0)
 
 
 # round up 2011 roundtime to 30 min interval 
@@ -96,24 +113,75 @@ loss %>%
   mutate(diff = c(1800, diff(roundtime))/60) -> loss
 
 # an more time duplicate check
-loss %>%
-  filter(diff == 0)
+loss %>% filter(diff == 0)
 loss[which(loss$diff == 0) + (-2:2),]
-loss <- loss[-which(loss$diff == 0), ]
+which(loss$diff == 0) -> tempo 
+loss[tempo, "roundtime"] <- loss[tempo, "roundtime"] + duration(mins = 30)
+loss$diff <- NULL
 
 
-
+# TRANSFORM dataframe ========================================
 loss %>% 
-  select(plot, year, roundtime, n_loss) %>%
-  spread(key = plot, value = n_loss) -> loss_n
+  gather(variable, value, drainage_mm:n_loss) %>% 
+  unite(temp, plot, variable, sep = " ") %>%
+  spread(temp, value) -> serf_loss
 
-loss %>% 
-  select(plot, year, roundtime, drainage_mm) %>%
-  spread(key = plot, value = drainage_mm) -> loss_d
+serf_loss %>% 
+  group_by(year) %>% 
+  summarize_at(vars(matches("n_loss")), sum, na.rm = TRUE)
 
 
-na.approx(loss_n$`Plot 2`, loss_n$roundtime, na.rm = FALSE, maxgap = 3) -> loss_n$p2n
-na.approx(loss_n$`Plot 3`, loss_n$roundtime, na.rm = FALSE, maxgap = 3) -> loss_n$p3n
-na.approx(loss_n$`Plot 4`, loss_n$roundtime, na.rm = FALSE, maxgap = 3) -> loss_n$p4n
-na.approx(loss_n$`Plot 5`, loss_n$roundtime, na.rm = FALSE, maxgap = 3) -> loss_n$p5n
-na.approx(loss_n$`Plot 6`, loss_n$roundtime, na.rm = FALSE, maxgap = 3) -> loss_n$p6n
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # interpolate if gap is 3
+
+serf_loss %>% filter(year == 2011) -> tempo
+na.approx(tempo$`plot2 n_loss`, tempo$roundtime, na.rm = FALSE, maxgap = 1) %>% sum(na.rm = T)
+
+
+## DOES NOT WORK
+# serf_loss %>%
+#   group_by(year) %>%
+# 
+#   mutate(plot2_n_loss = na.approx(`plot2 n_loss`, roundtime, na.rm = FALSE, maxgap = 3)) %>%
+#   mutate(plot3_n_loss = na.approx(`plot3 n_loss`, roundtime, na.rm = FALSE, maxgap = 3)) %>%
+#   mutate(plot4_n_loss = na.approx(`plot4 n_loss`, roundtime, na.rm = FALSE, maxgap = 3)) %>%
+#   mutate(plot5_n_loss = na.approx(`plot5 n_loss`, roundtime, na.rm = FALSE, maxgap = 3)) %>%
+#   mutate(plot6_n_loss = na.approx(`plot6 n_loss`, roundtime, na.rm = FALSE, maxgap = 3)) %>%
+# 
+#   mutate(plot2_flow = na.approx(`plot2 drainage_mm`, roundtime, na.rm = FALSE, maxgap = 3)) %>%
+#   mutate(plot3_flow = na.approx(`plot3 drainage_mm`, roundtime, na.rm = FALSE, maxgap = 3)) %>%
+#   mutate(plot4_flow = na.approx(`plot4 drainage_mm`, roundtime, na.rm = FALSE, maxgap = 3)) %>%
+#   mutate(plot5_flow = na.approx(`plot5 drainage_mm`, roundtime, na.rm = FALSE, maxgap = 3)) %>%
+#   mutate(plot6_flow = na.approx(`plot6 drainage_mm`, roundtime, na.rm = FALSE, maxgap = 3)) %>%
+# 
+#   select(roundtime, year,
+#          plot2_flow, plot2_n_loss,
+#          plot3_flow, plot3_n_loss,
+#          plot4_flow, plot4_n_loss,
+#          plot5_flow, plot5_n_loss,
+#          plot6_flow, plot6_n_loss) %>%
+# 
+#   ungroup()-> a
+# 
+# a %>%
+#   group_by(year) %>%
+#   summarize_at(vars(matches("n_loss")), sum, na.rm = TRUE)
+## DOES NOT WORK
+
+
